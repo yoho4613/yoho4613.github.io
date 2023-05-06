33f95678cbe1234acf14effd803bc5d935b65281
박지호 <yoho4613@gmail.com>
2023-05-06 22:10:01 +1200

    recent projects section updated

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,77 @@
+// Projects Detail
+const projectDetails = [
+  {
+    title: "TradeMe Manager App",
+    subtitle: "Full Stack",
+    description:
+      "Trademe-Manager provides the tool for Trademe Sellers. TradeMe-Manager uses all Trademe APIs, making it easy for you. Profile Authentication by API key Trademe-Manager is working on tmsandbox (dev Server) Server. Stacks .",
+    stack:
+      "Next.js (Framework) Tailwind (Styling) AlanAI (voice assistant) NodeMailer, Knex (Subscription) Netlify (Deploy) Auth1.0 (Authentication)",
+    howToUse:
+      "Login or Register with tmsandbox api key and manage your tradeMe listing",
+    images: ["project-TradeMe-Manager.jpg"],
+    url: "https://trademe-manager.netlify.app",
+  },
+  {
+    title: "E-commerce Website",
+    subtitle: "Full Stack",
+    description:
+      "This is basic e-Commerce website. App provides banner, user cart, online payment, shipping detail. Admin can create new products, update and delete products.",
+    stack:
+      "Next.js(framework), Sanity(admin dashboard), stripe(payment) This App can be improved by adding categories, Authentication, tracking.",
+    howToUse: "Choose the products you want and make test payment via cart.",
+    images: ["project-ecommerce.jpg"],
+    url: "https://ecommerce-jiho.vercel.app/",
+  },
+  {
+    title: "Web3 Minting Website",
+    subtitle: "Frontend, Smart Contract",
+    description:
+      "Our website is a user-friendly platform that allows you to easily mint your own NFTs through our secure smart contract. With our landing page, you can quickly navigate to our minting page where you can create unique and one-of-a-kind NFTs to showcase your creativity and imagination. Our platform is designed to be simple and intuitive, so anyone can participate in the world of NFTs.",
+    stack:
+      "Solidity (Smart Contract), React (front-end), web3js (Interact with the Ethereum)",
+    howToUse:
+      "Install MetaMask wallet extension and connect on Mint page. You will be able to mint NFTs",
+    images: ["project-clbplayers.jpg"],
+    url: "https://clbplayers.com",
+  },
+  {
+    title: "Movie Discover Website",
+    subtitle: "Frontend",
+    description:
+      "FilmPire is the web app that user can discover movie, TV shows. The app uses TMDB API to get data and user can login with TMDB credencials. User can find details of movies and TV shows by search, categories, genres.",
+    stack:
+      "Next.js (framework) MUI (styling component) Alan-AI (voice assistant)",
+    howToUse: "Login or register via TMDB and search or browse movies you want",
+    images: ["project-FilmPire.jpg"],
+    url: "https://firmpaire-jiho.netlify.app/",
+  },
+  {
+    title: "Self-Improvement App",
+    subtitle: "Full Stack (Group Project)",
+    description:
+      "S.I.P (Self ImProvement) is Trello style Todo list App. App provides user basic todo list features and period so user can create his/her life cycle. App also provides location setting, success rate chart.",
+    stack:
+      "Google Map API, React Drag & Drop(react-dnd), Chart.js Knex App could be improved by develop Mobile App version if it is necessary.",
+    howToUse:
+      "Create category and cards and fill the detail (title, description, address, period). App will record your schdule and check your progress",
+    images: ["project-SIP.jpg"],
+    url: "https://github.com/harakeke-2023/sip",
+  },
+  {
+    title: "Booking & online Order App",
+    subtitle: "Full Stack",
+    description:
+      "This is Web App that have Booking and online order with payment system. It also have seperate admin dashboard with check bookings, paid order list, update opening hours and date, update menu items. The app provides user's own Database.",
+    stack:
+      "Google Map API, React Drag & Drop(react-dnd), Chart.js Knex App could be improved by develop Mobile App version if it is necessary.",
+    howToUse:
+      "AWS (S3), Stripe (payment), Prisma(database), Next.js-T3 (framework), Vercel (deploy)",
+    images: ["project-Booking-App.jpg"],
+    url: "https://https://restaurant-booking-t3.vercel.app/",
+  },
+];
+
 // -----------SHOW MENU----------- //
 const navMenu = document.getElementById("nav-menu"),
   navToggle = document.getElementById("nav-toggle"),
@@ -110,34 +184,137 @@ const sr = ScrollReveal({
   // reset: true //Animations repeat
 });
 
-// Projects Description
-const projectsDesc = document.querySelectorAll('.projects__desc')
-const projectBtn = document.querySelectorAll('.project__desc-btn')
-const originalDesc = [];
+// Projects Details
+const projectsContainer = document.querySelector(".projects__container");
 
+projectDetails.forEach((project, i) => {
+  const card = document.createElement("div");
+  card.classList.add("projects__card-container");
+  card.innerHTML = `
+ <h2 class="projects__title">${project.title}</h2>
+ <span class="projects__subtitle">${project.subtitle}</span>
+ <article class="projects__card">
+   <button class="projects__button detail-btn" id="projects-button-${i}">
+     <img
+       src="./src/img/${project.images[0]}"
+       alt="projects image"
+       class="projects__img"
+     />
+   </button>
+ </article>
+ <div class="projects__buttons">
+   <a href=${project.url} target="_blank" class="projects__button">
+     View demo <i class="ri-external-link-line"></i>
+   </a>
+   <button class="projects__button detail-btn" id="projects-button-${i}">
+     View Details <i class="ri-external-link-line"></i>
+   </button>
+ </div>
+ `;
+  projectsContainer.appendChild(card);
+});
 
-projectsDesc.forEach((desc) => {
-  originalDesc.push(desc.innerHTML)
-  desc.innerHTML = `${desc.innerHTML.slice(17, 57)}.....`
-})
+// closeBtn.addEventListener("click", () => {
+//   detailContainer.classList.add("hidden");
+//   removeDetails();
+// });
 
-projectBtn.forEach((btn, i) => {
-  btn.addEventListener('click', () => {
-    if(btn.textContent === 'show more') {
-      projectsDesc[i].innerHTML = originalDesc[i]
-      btn.innerHTML = 'show less'
-    }  else {
-      projectsDesc[i].innerHTML = `${originalDesc[i].slice(17, 57)}.....`
-      btn.innerHTML = 'show more'
-    }
-  })
-})
+const projectBtn = document.querySelectorAll(".detail-btn");
+const detailContainer = document.querySelector(".projects__detail");
+const closeBtn = document.querySelector(".close-btn");
 
+function removeDetails() {
+  detailContainer.innerHTML = "";
+}
+detailContainer.addEventListener("click", (e) => {
+  if ([...e.target.classList].some((el) => el === "projects__detail")) {
+    detailContainer.classList.add("hidden");
+    removeDetails();
+  }
+});
+// Arrow Button Icon
+//  <svg
+// class="arrow-btn left"
+// xmlns="http://www.w3.org/2000/svg"
+// width="16"
+// height="16"
+// fill="currentColor"
+// class="w-5 h-5 text-red-500 bi bi-chevron-left dark:text-red-200"
+// viewBox="0 0 16 16"
+// >
+// <path
+//   fillRule="evenodd"
+//   d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
+// ></path>
+// </svg>
+// <svg
+// class="arrow-btn right"
+// xmlns="http://www.w3.org/2000/svg"
+// width="16"
+// height="16"
+// fill="currentColor"
+// class="w-5 h-5 text-red-500 bi bi-chevron-right dark:text-red-200"
+// viewBox="0 0 16 16"
+// >
+// <path
+//   fillRule="evenodd"
+//   d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+// ></path>
+// </svg> 
 
-sr.reveal(`.home__data, .home__social, .contact__container, .footer__container`);
+projectBtn.forEach((desc) => {
+  const index = desc.id.charAt(desc.id.length - 1);
+
+  desc.addEventListener("click", () => {
+    detailContainer.innerHTML = `
+      <div class="projects__innerContainer">
+      <i class="ri-close-line close-btn"></i>
+      <h2 class="projects__title">${projectDetails[index].title}</h2>
+      <span class="projects__subtitle">${projectDetails[index].subtitle} </span>
+
+      <div class="relative">
+      <img src="/src/img/${projectDetails[index].images[0]}" />
+    </div>
+      <hr />
+      <h4>Description</h4>
+      <p class="projects__desc">
+      ${projectDetails[index].description}
+      </p>
+      <hr />
+      <h4>Stack</h4>
+      <p class="projects__desc">
+      ${projectDetails[index].stack}
+      </p>
+      <hr />
+      <h4>How to use</h4>
+      <p class="projects__desc">
+      ${projectDetails[index].howToUse}
+      </p>
+      <a
+      href="${projectDetails[index].url}"
+      target="_blank"
+      class="projects__button"
+      >
+      View demo <i class="ri-external-link-line"></i>
+      </a>
+      </div>
+      `;
+
+    setTimeout(() => {
+      document.querySelector(".projects__innerContainer").classList.add("show");
+    }, 0);
+    document.querySelector(".close-btn").addEventListener("click", () => {
+      detailContainer.classList.add("hidden");
+      removeDetails();
+    });
+    detailContainer.classList.remove("hidden");
+  });
+});
+
+sr.reveal(
+  `.home__data, .home__social, .contact__container, .footer__container`
+);
 sr.reveal(`.home__image`, { origin: "bottom" });
 sr.reveal(`.about__data, .skills__data`, { origin: "left" });
 sr.reveal(`.about__image, .skills__content`, { origin: "right" });
 sr.reveal(`.services__card, .projects__card-container`, { interval: 100 });
-
-
